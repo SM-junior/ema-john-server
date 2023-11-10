e2fbe8a6fc686281b29971dac22c816da4649ebf
SM-junior <shahinalam12341@gmail.com>
2023-11-11 00:54:16 +0600

    all data from db to client and pagination is done

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 3000;
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('ema is running')
+})
+
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.n2wd0zs.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const productsCollection = client.db("EmaJohnDB").collection("products");
+
+    //get all data from mongodb and set it to client site///////////////////////////////////
+    app.get('/products', async (req, res) => {
+      console.log(req.query);
+      const page = parseInt(req.query.page) || 0;
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = page * limit
+      const result = await productsCollection.find().skip(skip).limit(limit).toArray();
+      // const result = await productsCollection.find().toArray()  // this line receiving data from mongodb
+      res.send(result)  //this line is sending data to client side
+    })
+
+    app.get('/totalProducts', async (req, res) => {
+      const result = await productsCollection.estimatedDocumentCount();
+      res.send({ totalProducts: result })
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`ema is running on port ${port}`);
+})
